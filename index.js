e249e46f8f779db3f322a8c7214cca761be651e8
manju <manju@1234.com>
2024-02-12 14:52:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    window.location.replace("Kitchen.html");
+  }
+  function mygetfreeFunction() {
+    window.location.replace("error.html");
+  }
+  function myContactFunction() {
+    window.location.replace("contact.html");
+  }
+  function myContact2Function() {
+    window.location.replace("error.html");
+  }
+  function myerrorFunction() {
+    window.location.replace("Homepage.html");
+  }
+  function ourprojectFunction() {
+    window.location.replace("ourproject.html");
+  }
